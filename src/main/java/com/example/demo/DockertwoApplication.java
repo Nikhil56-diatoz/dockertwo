f0aafd1325a08ac2020f0a50774d887327add3b8
Nikhil kumar <nikhil.kumar@diatoz.com>
2020-06-29 12:47:28 +0530

    first commit

---
+++ src/main/java/com/example/demo/DockertwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockertwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertwoApplication.class, args);
+	}
+
+}
